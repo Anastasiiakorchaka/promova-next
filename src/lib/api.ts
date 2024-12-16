e1a068450f1f6685565632cac3d429b4356ee080
Anastasiia Korchaka <anastasiiakorchaka@gmail.com>
2024-12-17 00:47:03 +0200

    commit

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 const API_URL = process.env.NODE_ENV === 'production' 
-  ? process.env.STRAPI_URL // для продакшн-среды
-  : process.env.BASE_API_URL; // для разработки
+  ? process.env.STRAPI_URL
+  : process.env.BASE_API_URL; 
 
 export async function fetchAPI(endpoint: string, options = {}) {
   const res = await fetch(`${API_URL}${endpoint}`, {
